32992189c9bb93464c24ec140d0e2b8d3089ab21
Pannaga_Bhushana <bhushana.pannaga@byjus.com>
2022-03-11 11:27:08 +0530

    code refactoring.

---
+++ AutoScrollMenu/View/FooterView.swift
@@ -0,0 +1,67 @@
+//
+//  FooterView.swift
+//  AutoScrollMenu
+//
+//  Created by Pannaga Bhushana on 10/03/22.
+//
+
+import SwiftUI
+
+extension StackView {
+    
+    struct FooterView: View {
+        @StateObject var viewModel: ViewModel
+        var viewAllButtonPressed = {}
+
+        var body: some View {
+            HStack{
+                HStack{
+                        Button(action: {
+                                    viewModel.moveCards(false)
+                            }) {
+                                
+                                Image("app_level_extra_small_arrow_left_transparent")
+                                .resizable()
+                                .renderingMode(.original)
+                                
+                                }
+                            .frame(width: 16, height: 16, alignment: .leading)
+                            
+                    Text("Previous")
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(Color.init(hex: "#8f8f8f"))
+                    
+                }
+                .opacity(viewModel.lastCardIndex == 2 ? 0: 1)
+
+                Spacer()
+                HStack {
+                    Text("\(viewModel.lastCardIndex - 1)\(Text("/\(String(StackView.ViewModel.datas.count))").foregroundColor(Color.init(hex: "#444444")))")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color.init(hex: "#c7c7c7"))
+                }
+                .offset(x: -20, y: 0)
+                    
+                Spacer()
+                Button("View all", action: {
+                    self.viewAllButtonPressed()
+                })
+                    .foregroundColor(Color.init(hex: "#ffb700"))
+                    .font(.system(size: 14, weight: .medium))
+        }
+            .padding()
+    }
+       
+        func viewAllButtonPressed(_ callback: @escaping () -> ()) -> some View {
+            FooterView(viewModel: viewModel, viewAllButtonPressed: callback)
+        }
+    }
+
+    struct FooterView_Previews: PreviewProvider {
+        static var previews: some View {
+            FooterView(viewModel: ViewModel())
+        }
+    }
+
+}
+
--- AutoScrollMenu/View/StackView.swift
+++ AutoScrollMenu/View/StackView.swift
@@ -38,45 +38,15 @@ struct StackView: View {
                             .offset(x: viewModel.isTopCard(data) ?viewModel.translation.width : 0, y: viewModel.getCardOffset(geometry, id: index))
                     }
                 }
-                Spacer()
+               
             }
-            
-            HStack{
-                HStack{
-                Button(action: {
-                    viewModel.moveCards(false)
-                            }) {
-                                
-                                Image("previous")
-                                .resizable()
-                                .renderingMode(.original)
-                                
-                }
-                            .frame(width: 16, height: 16, alignment: .leading)
-                            
-                    Text("Previous")
-                        .font(.system(size: 14, weight: .medium))
-                        .foregroundColor(Color.init(hex: "#8f8f8f"))
-                    
-                }
-                .opacity(viewModel.lastCardIndex == 2 ? 0: 1)
-
-                Spacer()
-                HStack {
-                    Text("\(viewModel.lastCardIndex - 1)\(Text("/\(String(StackView.ViewModel.datas.count))").foregroundColor(Color.init(hex: "#444444")))")
-                        .font(.system(size: 14))
-                        .foregroundColor(Color.init(hex: "#c7c7c7"))
-                }
-                .offset(x: -20, y: 0)
-                    
-                Spacer()
-                Button("View all", action: {
+            Spacer()
+            FooterView(viewModel: viewModel)
+                .viewAllButtonPressed {
                     self.viewAllButtonPressed?()
-                })
-                    .foregroundColor(Color.init(hex: "#ffb700"))
-                    .font(.system(size: 14, weight: .medium))
-            }
-            .padding()
+                }
+            
+        
         }.padding()
             .background(.white)
     }
